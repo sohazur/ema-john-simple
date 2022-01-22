21c1472120e7e63b4cdc2e892f9d1599950d2738
Sohazur <mdsohazurislam@gmail.com>
2022-01-22 06:21:49 +0400

    Made a new component within cart

---
+++ src/components/Cart/Cart.js
@@ -0,0 +1,19 @@
+import React from "react";
+import "./Cart.css";
+
+const Cart = (props) => {
+  const { cart } = props;
+  let total = 0;
+  for (const product of cart) {
+    total += product.price;
+  }
+  return (
+    <div>
+      <h3>Order Summary</h3>
+      <h5>Items Ordered: {props.cart.length}</h5>
+      <p>Total: ${total}</p>
+    </div>
+  );
+};
+
+export default Cart;
