377e75b049a608aaf73b7a1dd1a12ed01775cd67
Sohazur <mdsohazurislam@gmail.com>
2022-01-22 06:21:23 +0400

    Added the total price field

--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -13,7 +13,7 @@ const Product = (props) => {
         <p>
           <small>by: {seller}</small>
         </p>
-        <p>Price: {price}</p>
+        <p>Price: ${price}</p>
         <p>
           <small>Only {stock} left in stock - order soon</small>
         </p>
--- src/components/Shop/Shop.js
+++ src/components/Shop/Shop.js
@@ -1,16 +1,19 @@
 import React, { useEffect, useState } from "react";
+import Cart from "../Cart/Cart";
 import Product from "../Product/Product";
 import "./Shop.css";
 
 const Shop = () => {
   const [products, setProducts] = useState([]);
+  const [cart, setCart] = useState([]);
   useEffect(() => {
     fetch("./products.JSON")
       .then((res) => res.json())
       .then((data) => setProducts(data));
   }, []);
   const handleAddToCart = (product) => {
-    console.log(product);
+    const newCart = [...cart, product];
+    setCart(newCart);
   };
   return (
     <div className="shop-container">
@@ -24,8 +27,7 @@ const Shop = () => {
         ))}
       </div>
       <div className="cart-container">
-        <h3>Order Summary</h3>
-        <h5>Items Ordered: </h5>
+        <Cart cart={cart}></Cart>
       </div>
     </div>
   );
