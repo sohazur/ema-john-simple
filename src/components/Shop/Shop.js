c693a1b9e1b0e378bd9d788fcd04431693b82114
Sohazur <mdsohazurislam@gmail.com>
2022-01-23 03:48:15 +0400

    Adding items to save in local storage

--- src/components/Shop/Shop.js
+++ src/components/Shop/Shop.js
@@ -2,6 +2,8 @@ import React, { useEffect, useState } from "react";
 import Cart from "../Cart/Cart";
 import Product from "../Product/Product";
 import "./Shop.css";
+import "../../utilities/fakedb";
+import { addToDb } from "../../utilities/fakedb";
 
 const Shop = () => {
   const [products, setProducts] = useState([]);
@@ -14,6 +16,7 @@ const Shop = () => {
   const handleAddToCart = (product) => {
     const newCart = [...cart, product];
     setCart(newCart);
+    addToDb(product.key);
   };
   return (
     <div className="shop-container">
