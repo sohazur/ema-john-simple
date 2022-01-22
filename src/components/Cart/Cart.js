627fa8632a471066ea6a4213328b54a4b4380bef
Sohazur <mdsohazurislam@gmail.com>
2022-01-22 06:45:12 +0400

    Fixed total price

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -3,12 +3,12 @@ import "./Cart.css";
 
 const Cart = (props) => {
   const { cart } = props;
-  const total = cart.reduce((previous, product) => previous + product.price);
+  const total = cart.reduce((previous, product) => previous + product.price, 0);
   return (
     <div>
       <h3>Order Summary</h3>
       <h5>Items Ordered: {props.cart.length}</h5>
-      <p>Total: ${total}</p>
+      <p>Total: ${total.toFixed(2)}</p>
     </div>
   );
 };
